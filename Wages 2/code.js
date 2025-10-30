2ff35692a7f5ef1fa99469b974b7b4890cfce044
gkriech15 <kriechg85@gmail.com>
2025-10-30 12:34:50 -0500

    Add files via upload

---
+++ Wages 2/code.js
@@ -0,0 +1,16 @@
+document.getElementById ("button"). addEventListener("click", oneclick)
+
+function oneclick () {
+    let normal_hours = document.getElementById("nh").valueAsNumber
+    let normal_rate = document.getElementById("nr").valueAsNumber
+    let overtime_rate = document.getElementById("or").valueAsNumber
+    let hours_worked = document.getElementById("hw").valueAsNumber
+    let wage
+    if (hours_worked <= normal_hours) {// no overtime
+        wage = hours_worked*normal_rate
+    } else {
+        wage = normal_hours*normal_rate+(hours_worked-normal_hours)*overtime_rate
+
+    }
+    document.getElementById("result").innerHTML = wage + " dollars"
+}
