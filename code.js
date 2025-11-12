2aeafda9cfd2ff6b0d6a4ac8b137c4300dbcb7c8
gkriech15 <kriechg85@gmail.com>
2025-11-12 13:31:19 -0600

    Add files via upload

--- code.js
+++ code.js
@@ -1,15 +1,20 @@
-document.getElementById("btn").addEventListener("click", onclick)
-
-function onclick (e) {//callback function
-    let p = document.getElementById("p").valueAsNumber
-    let n = 12*document.getElementById("year").valueAsNumber
-    let r = document.getElementById("ARate").valueAsNumber /100 / 12
-    let A = p * r * (1+r)**n / ((1+r)**n-1)
-
-    let result = "The monthly payment is $" + A.toFixed (2) +
-            "<br>The total payment is $" + (A*n).toFixed(2) +
-            "<br>The total interest is $" + (A*n - p).toFixed(2)
-
-    document.getElementById("result").innerHTML = result;
+let pen = document.getElementById("c").getContext("2d");
+drawBackground()
+// how to repeat the while-body N times
+let N = 13
+let i = 0
+while (i < N) {
+    drawSquare(10+i*30, 10+12*30-i*30)
+    ++i; // i = i + 1; i += 1, i++
 }
 
+function drawBackground () {
+  pen.fillStyle = "#aaaaaa"
+  pen.fillRect(0, 0, 400, 400)  
+} 
+
+function drawSquare (x, y) {
+    pen.fillStyle = "#0000ffff" // variable for the color
+    pen.fillRect(x, y, 20, 20)
+
+}
