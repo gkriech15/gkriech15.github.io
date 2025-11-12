2cf1806d5bfcfaf307fa67f3ce3422ca218459ac
gkriech15 <kriechg85@gmail.com>
2025-11-12 16:39:05 -0600

    Add files via upload

--- code.js
+++ code.js
@@ -1,20 +1,16 @@
-let pen = document.getElementById("c").getContext("2d");
-drawBackground()
-// how to repeat the while-body N times
-let N = 13
-let i = 0
-while (i < N) {
-    drawSquare(10+i*30, 10+i*30)
-    ++i; // i = i + 1; i += 1, i++
-}
+document.getElementById("btn").addEventListener("click", oneclick)
 
-function drawBackground () {
-  pen.fillStyle = "#aaaaaa"
-  pen.fillRect(0, 0, 400, 400)  
+function oneclick ()
+{
+    num1 = document.getElementById("first").valueAsNumber;
+    num2 = document.getElementById("second").valueAsNumber;
+    num3 = document.getElementById("third").valueAsNumber;
+    let max = num1;
+    if (num2 > max) {
+        max = num2;
+    }
+    if (num3 > max) {
+        max = num3;
+    }
+    document.getElementById("result").innerHTML = max;
 } 
-
-function drawSquare (x, y) {
-    pen.fillStyle = "#0000ffff" // variable for the color
-    pen.fillRect(x, y, 20, 20)
-
-}
