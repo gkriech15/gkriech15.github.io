b544d2d78a5fda913e3e30e19b5f4e5fe77b50fe
gkriech15 <kriechg85@gmail.com>
2025-11-17 11:33:01 -0600

    Add files via upload

--- loops2/code.js
+++ loops2/code.js
@@ -1,20 +1,40 @@
 let pen = document.getElementById("b").getContext("2d");
-drawBackground()
+let pen2 = document.getElementById("c").getContext("2d");
+
+drawBackground();
+drawBackground2();
 // how to repeat the while-body N times
-let N = 13
-let i = 0
+let N = 13;
+let i = 0;
 while (i < N) {
-    drawSquare(10+i*30, 10+i*30)
+    drawSquare(10+i*30, 10+i*30);
     ++i; // i = i + 1; i += 1, i++
 }
-
+let j = 0;
+while (j < N) {
+    drawSquare2 (10+j*30, 10+j*30)
+    drawSquare2(10 + j * 30, 10 + (N - 1 - j) * 30);
+    ++j; // i = i + 1; i += 1, i++
+}
 function drawBackground () {
-  pen.fillStyle = "#aaaaaa"
-  pen.fillRect(0, 0, 400, 400)  
+  pen.fillStyle = "#aaaaaa";
+  pen.fillRect(0, 0, 400, 400);  
+} 
+
+function drawBackground2 () {
+  pen2.fillStyle = "#aaaaaa";
+  pen2.fillRect(0, 0, 400, 400);  
 } 
 
 function drawSquare (x, y) {
-    pen.fillStyle = "#0000ffff" // variable for the color
-    pen.fillRect(x, y, 20, 20)
+    pen.fillStyle = "#0000ffff"; // variable for the color
+    pen.fillRect(x, y, 20, 20);
 
 }
+
+function drawSquare2 (x, y) {
+    pen2.fillStyle = "#0000ffff"; // variable for the color
+    pen2.fillRect(x, y, 20, 20);
+
+}
+
